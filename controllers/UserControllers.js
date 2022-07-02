180792cc284b70857df92dece69a1177ac13f853
zpengineer <clickprime94@gmail.com>
2022-07-02 16:23:56 +0300

    Added logout route

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -94,6 +94,19 @@ export const login = async (req, res) => {
     }
 };
 
+export const logout = async (req, res) => {
+    try {
+        
+        
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Не удалось разлогиниться",
+        });
+    }
+};
+
 export const current = async (req, res) => {
     try {
 
--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    token: {
+        type: String,
+        default: null,
+    },
     avatarUrl: String,
 }, {
     timestamps: true,
