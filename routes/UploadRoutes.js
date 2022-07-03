f2b25c49f148410e59c5cb8831695b28a2bf7534
zpengineer <clickprime94@gmail.com>
2022-07-03 09:13:47 +0300

    Create uploads folder

--- routes/UploadRoutes.js
+++ routes/UploadRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
+import fs from "fs";
 import multer from "multer";
 
 import checkAuth from "../utils/CheckAuth.js";
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+
+        if (!fs.existsSync('uploads')) {
+        fs.mkdirSync('uploads');
+        }
+        
         cb(null, 'uploads');
     },
     filename: (_, file, cb) => {
