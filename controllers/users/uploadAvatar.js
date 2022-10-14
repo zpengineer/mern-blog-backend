efbe342e1a4e0eb092a83256044db9746604f6af
Oleh Fedorov <clickprime94@gmail.com>
2022-10-14 19:21:58 +0300

    Refactoring upload avatar

--- controllers/users/uploadAvatar.js
+++ controllers/users/uploadAvatar.js
@@ -1,25 +1,15 @@
-const {cloudinary} = require('../../utils/cloudinary');
 const {User} = require('../../models');
 
 const uploadAvatar = async (req, res, next) => {
-  const {path: tmpUpload} = req.file;
+  const {avatar} = req.body;
+
+  console.log(req.body);
 
   const {_id: id} = req.user;
-  const userFolder = `avatars/${id}`;
 
-  try {
-    const uploadResponse = await cloudinary.uploader.upload(tmpUpload, {
-      folder: userFolder,
-      transformation: {width: 250, height: 250, crop: 'fill'},
-    });
+  await User.findByIdAndUpdate(id, {avatarURL: avatar}, {new: true});
 
-    await User.findByIdAndUpdate(id, {avatarURL: uploadResponse.secure_url});
-
-    res.status(200).json({avatarURL: uploadResponse.secure_url});
-  } catch (error) {
-    await fs.unlink(tmpUpload);
-    next(error);
-  }
+  res.status(200).json({message: 'success'});
 };
 
 module.exports = uploadAvatar;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = new express.Router();
 
-const {ctrlWrapper, checkAuth, upload} = require('../../middlewares');
+const {ctrlWrapper, checkAuth} = require('../../middlewares');
 const {users} = require('../../controllers');
 
 router.get('/current', checkAuth, ctrlWrapper(users.current));
 
 router.get('/author/:postId', ctrlWrapper(users.author));
 
-router.patch(
-    '/avatars',
-    checkAuth,
-    upload.single('avatar'),
-    ctrlWrapper(users.uploadAvatar),
-);
+router.patch('/avatars', checkAuth, ctrlWrapper(users.uploadAvatar));
 
 router.patch('/follow/:id', checkAuth, ctrlWrapper(users.follow));
 
